5d880a2401a449834dc033b4fa3a19dc7a593715
minh711 <duyminhtruong711@gmail.com>
2024-01-25 15:52:27 +0700

    Simple List Header UI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import ListHeader from './components/ListHeader'
+
 const App = () => {
   return (
-    <div>
+    <div className='app'>
+      <ListHeader listName={'🌴 Holiday tick list'}/>
     </div>
   );
 }
--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const ListHeader = () => {
+const ListHeader = ({ listName }) => {
+  const signOut = () => {
+    console.log('Sign Out');
+  }
+  
   return (
-    <div>ListHeader</div>
+    <div className='list-header'>
+      <h1>{listName}</h1>
+      <div className='button-container'>
+        <button className='create'>ADD NEW</button>
+        <button className='signout' onClick={signOut}>SIGN OUT</button>
+      </div>
+    </div>
   )
 }
 
